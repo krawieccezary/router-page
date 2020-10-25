2049e36d6d253493326306d9eb555ec5dd07d690
krawieccezary <web@cezarykrawiec.pl>
2020-10-25 14:54:40 +0100

    Updates

---
+++ precache-manifest.d14245dd41e4e642bf5440dcbb9df9af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427cd8143713272b8399908f09c0b808",
+    "url": "/router-page/index.html"
+  },
+  {
+    "revision": "08fae03e1b38e158232b",
+    "url": "/router-page/static/css/main.03f21a3b.chunk.css"
+  },
+  {
+    "revision": "1c8b4a6647a825d2e935",
+    "url": "/router-page/static/js/2.c487eb0a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/router-page/static/js/2.c487eb0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08fae03e1b38e158232b",
+    "url": "/router-page/static/js/main.bac7b9e4.chunk.js"
+  },
+  {
+    "revision": "c08447b7efbbceee9a9b",
+    "url": "/router-page/static/js/runtime-main.ebd0d6d5.js"
+  },
+  {
+    "revision": "d788a630957fbde3929898fe34b80415",
+    "url": "/router-page/static/media/dave-hoefler-lsoogGC_5dg-unsplash.d788a630.jpg"
+  },
+  {
+    "revision": "1fac6351fc60ae66631f7e4128088575",
+    "url": "/router-page/static/media/jay-mantri-TFyi0QOx08c-unsplash.1fac6351.jpg"
+  },
+  {
+    "revision": "8b3028c2b2e1ab3a2898f04b1ee988ed",
+    "url": "/router-page/static/media/kunal-shinde--f0YLss50Bs-unsplash.8b3028c2.jpg"
+  },
+  {
+    "revision": "6af7a9a234dbca0465939c7b6efe81c5",
+    "url": "/router-page/static/media/matthew-brodeur-V9nLp8zE_eQ-unsplash.6af7a9a2.jpg"
+  }
+]);
